707b6c8dc8a189c603a9aa1e661f928e0624a109
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-07-28 16:08:05 +0300

    Updates

---
+++ precache-manifest.e9802c815028f94240991e55d5f4a86b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd39306bdeb027e56ac5a4d0730b3cd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e3642ed49ca1f6089990",
+    "url": "./static/css/main.067d0f9b.chunk.css"
+  },
+  {
+    "revision": "9dcc53d26a285ad4ed16",
+    "url": "./static/js/2.40510c7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.40510c7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3642ed49ca1f6089990",
+    "url": "./static/js/main.5404c23f.chunk.js"
+  },
+  {
+    "revision": "1610068e25a4810046d0",
+    "url": "./static/js/runtime-main.e1f50cee.js"
+  },
+  {
+    "revision": "5d19c6c1410e5f0985fda18299d1688c",
+    "url": "./static/media/Banner.5d19c6c1.png"
+  },
+  {
+    "revision": "efd54e736e9b6208710472c9c1479659",
+    "url": "./static/media/Mont-Bold.efd54e73.ttf"
+  },
+  {
+    "revision": "abb74a597efa488c9c0e5da1cf8f8570",
+    "url": "./static/media/Mont-Regular.abb74a59.ttf"
+  },
+  {
+    "revision": "3a51a487fbf488e842e1c9e48ec0515a",
+    "url": "./static/media/Phones.3a51a487.png"
+  },
+  {
+    "revision": "fb001c323878460dbb1b75e002cc02dd",
+    "url": "./static/media/Phones1.fb001c32.png"
+  },
+  {
+    "revision": "a3261dffea3477d49e32f89c1d62d8c1",
+    "url": "./static/media/Phones2.a3261dff.png"
+  },
+  {
+    "revision": "2eaae658b30f1671d0b3b0ed715dc797",
+    "url": "./static/media/Photo.2eaae658.png"
+  },
+  {
+    "revision": "208e513e18de2f73edad07d9dc47e07c",
+    "url": "./static/media/brand1.208e513e.png"
+  },
+  {
+    "revision": "88b893940921976044f6450d469ed54a",
+    "url": "./static/media/brand4.88b89394.png"
+  },
+  {
+    "revision": "74cfeb08aa00d99101fa8867810b03a2",
+    "url": "./static/media/hot1.74cfeb08.png"
+  },
+  {
+    "revision": "3b1fcf07cb8851b51c6f2397c3a22919",
+    "url": "./static/media/hot2.3b1fcf07.png"
+  },
+  {
+    "revision": "cd8c2195ac8812d28c0ce6943504b2de",
+    "url": "./static/media/hot3.cd8c2195.png"
+  },
+  {
+    "revision": "74cfeb08aa00d99101fa8867810b03a2",
+    "url": "./static/media/hot4.74cfeb08.png"
+  },
+  {
+    "revision": "e80046fae67b5bcf3feffbc6baac09b9",
+    "url": "./static/media/like.e80046fa.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f96163bf66b4bc18893bcedc683b7c29.js"
+  "./precache-manifest.e9802c815028f94240991e55d5f4a86b.js"
 );
 
 self.addEventListener('message', (event) => {
